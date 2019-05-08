0bea80b2cc2a386769a6c942bdbd6dd77b25a39a
wahyuridho <49488222+wahyuridho@users.noreply.github.com>
2019-05-09 01:37:37 +0700

    Update Manusia.java

--- TugasPBO/src/tugaspbo/Manusia.java
+++ TugasPBO/src/tugaspbo/Manusia.java
@@ -3,6 +3,7 @@ package tugaspbo;
 public class Manusia extends RAS {
 // Overiding methode dari parent CLASS
 
+    @Override
     public void Berjalan() {
         System.out.println(" Jalan-Jalan Mulu. gak punya rumah ya?");
     }
