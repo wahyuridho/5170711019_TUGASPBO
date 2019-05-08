683724d081803913e55cb617f8626932808479ea
wahyuridho <49488222+wahyuridho@users.noreply.github.com>
2019-05-09 01:36:58 +0700

    Update Iblis.java

--- TugasPBO/src/tugaspbo/Iblis.java
+++ TugasPBO/src/tugaspbo/Iblis.java
@@ -4,6 +4,7 @@ public class Iblis extends RAS {
     public void Makan(){
         System.out.println(" Makan Terusss");
     }
+    @Override
     public void Goda(){
         super.Goda();
         System.out.println(" Goda Yang Cakep-Cakep");
